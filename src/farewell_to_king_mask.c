b20ada11a1b8eda0c047f7348030cf6826a5f7d6
Edward Sandor <public+git@sandorlaboratories.com>
2023-05-20 21:51:36 -0600

    Lookup table for King move mask

--- src/farewell_to_king_mask.c
+++ src/farewell_to_king_mask.c
@@ -152,6 +152,18 @@ static const ftk_board_mask_t knight_move_mask[FTK_STD_BOARD_SIZE] =
   0x0004020000000000, 0x0008050000000000, 0x00110a0000000000, 0x0022140000000000, 0x0044280000000000, 0x0088500000000000, 0x0010a00000000000, 0x0020400000000000, 
 };
 
+static const ftk_board_mask_t king_move_mask[FTK_STD_BOARD_SIZE] = 
+{
+  0x0000000000000302, 0x0000000000000705, 0x0000000000000e0a, 0x0000000000001c14, 0x0000000000003828, 0x0000000000007050, 0x000000000000e0a0, 0x000000000000c040, 
+  0x0000000000030203, 0x0000000000070507, 0x00000000000e0a0e, 0x00000000001c141c, 0x0000000000382838, 0x0000000000705070, 0x0000000000e0a0e0, 0x0000000000c040c0, 
+  0x0000000003020300, 0x0000000007050700, 0x000000000e0a0e00, 0x000000001c141c00, 0x0000000038283800, 0x0000000070507000, 0x00000000e0a0e000, 0x00000000c040c000, 
+  0x0000000302030000, 0x0000000705070000, 0x0000000e0a0e0000, 0x0000001c141c0000, 0x0000003828380000, 0x0000007050700000, 0x000000e0a0e00000, 0x000000c040c00000, 
+  0x0000030203000000, 0x0000070507000000, 0x00000e0a0e000000, 0x00001c141c000000, 0x0000382838000000, 0x0000705070000000, 0x0000e0a0e0000000, 0x0000c040c0000000, 
+  0x0003020300000000, 0x0007050700000000, 0x000e0a0e00000000, 0x001c141c00000000, 0x0038283800000000, 0x0070507000000000, 0x00e0a0e000000000, 0x00c040c000000000, 
+  0x0302030000000000, 0x0705070000000000, 0x0e0a0e0000000000, 0x1c141c0000000000, 0x3828380000000000, 0x7050700000000000, 0xe0a0e00000000000, 0xc040c00000000000, 
+  0x0203000000000000, 0x0507000000000000, 0x0a0e000000000000, 0x141c000000000000, 0x2838000000000000, 0x5070000000000000, 0xa0e0000000000000, 0x40c0000000000000, 
+};
+
 static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_board_mask_t board_mask, ftk_board_mask_t opponent_mask, ftk_square_e position, ftk_square_e *ep)
 {
   ftk_board_mask_t mask = 0;
@@ -182,10 +194,13 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
   {
     mask |= (knight_move_mask[position] & ((~board_mask) | opponent_mask));
   } 
-  else if ((square.type == FTK_TYPE_BISHOP) ||
-             (square.type == FTK_TYPE_ROOK) ||
-             (square.type == FTK_TYPE_QUEEN) ||
-             (square.type == FTK_TYPE_KING)) 
+  else if (square.type == FTK_TYPE_KING)
+  {
+    mask |= (king_move_mask[position] & ((~board_mask) | opponent_mask));
+  }
+  else if ( (square.type == FTK_TYPE_BISHOP) ||
+            (square.type == FTK_TYPE_ROOK)   ||
+            (square.type == FTK_TYPE_QUEEN) )
   {
     const ftk_board_mask_t movable_mask = ((~board_mask) | opponent_mask);
     if (square.type != FTK_TYPE_ROOK) 
@@ -197,8 +212,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
              ((test_mask & movable_mask) != 0))
       {
         mask |= test_mask;
-        if (square.type == FTK_TYPE_KING ||
-            (board_mask & 1ULL << test) != 0)
+        if ((board_mask & test_mask) != 0)
           break;
         test += 9;
         test_mask = FTK_POSITION_TO_MASK(test);
@@ -210,8 +224,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
              ((test_mask & movable_mask) != 0))
       {
         mask |= test_mask;
-        if (square.type == FTK_TYPE_KING ||
-            (board_mask & 1ULL << test) != 0)
+        if ((board_mask & test_mask) != 0)
           break;
         test -= 9;
         test_mask = FTK_POSITION_TO_MASK(test);
@@ -223,8 +236,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
              ((test_mask & movable_mask) != 0))
       {
         mask |= test_mask;
-        if (square.type == FTK_TYPE_KING ||
-            (board_mask & 1ULL << test) != 0)
+        if ((board_mask & test_mask) != 0)
           break;
         test += 7;
         test_mask = FTK_POSITION_TO_MASK(test);
@@ -236,8 +248,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
              ((test_mask & movable_mask) != 0))
       {
         mask |= test_mask;
-        if (square.type == FTK_TYPE_KING ||
-            (board_mask & 1ULL << test) != 0)
+        if ((board_mask & test_mask) != 0)
           break;
         test -= 7;
         test_mask = FTK_POSITION_TO_MASK(test);
@@ -251,8 +262,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
              ((test_mask & movable_mask) != 0))
       {
         mask |= test_mask;
-        if (square.type == FTK_TYPE_KING ||
-            (board_mask & 1ULL << test) != 0)
+        if ((board_mask & test_mask) != 0)
           break;
         test += 1;
         test_mask = FTK_POSITION_TO_MASK(test);
@@ -264,8 +274,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
              ((test_mask & movable_mask) != 0))
       {
         mask |= test_mask;
-        if (square.type == FTK_TYPE_KING ||
-            (board_mask & 1ULL << test) != 0)
+        if ((board_mask & test_mask) != 0)
           break;
         test -= 1;
         test_mask = FTK_POSITION_TO_MASK(test);
@@ -276,8 +285,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
              ((test_mask & movable_mask) != 0))
       {
         mask |= test_mask;
-        if (square.type == FTK_TYPE_KING ||
-            (board_mask & 1ULL << test) != 0)
+        if ((board_mask & test_mask) != 0)
           break;
         test += 8;
         test_mask = FTK_POSITION_TO_MASK(test);
@@ -288,8 +296,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
              ((test_mask & movable_mask) != 0))
       {
         mask |= test_mask;
-        if (square.type == FTK_TYPE_KING ||
-            (board_mask & 1ULL << test) != 0)
+        if ((board_mask & test_mask) != 0)
           break;
         test -= 8;
         test_mask = FTK_POSITION_TO_MASK(test);

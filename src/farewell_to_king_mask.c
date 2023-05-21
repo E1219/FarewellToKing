a9947160d8acf0a1f9fedf9ff04e43f6eb651f0a
Edward Sandor <public+git@sandorlaboratories.com>
2023-05-20 21:00:48 -0600

    Look-up table to generate Knight moves

--- src/farewell_to_king_mask.c
+++ src/farewell_to_king_mask.c
@@ -140,6 +140,18 @@ static const ftk_board_mask_t black_pawn_capture_mask[FTK_STD_BOARD_SIZE] =
   0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 
 };
 
+static const ftk_board_mask_t knight_move_mask[FTK_STD_BOARD_SIZE] = 
+{
+  0x0000000000020400, 0x0000000000050800, 0x00000000000a1100, 0x0000000000142200, 0x0000000000284400, 0x0000000000508800, 0x0000000000a01000, 0x0000000000402000, 
+  0x0000000002040004, 0x0000000005080008, 0x000000000a110011, 0x0000000014220022, 0x0000000028440044, 0x0000000050880088, 0x00000000a0100010, 0x0000000040200020, 
+  0x0000000204000402, 0x0000000508000805, 0x0000000a1100110a, 0x0000001422002214, 0x0000002844004428, 0x0000005088008850, 0x000000a0100010a0, 0x0000004020002040, 
+  0x0000020400040200, 0x0000050800080500, 0x00000a1100110a00, 0x0000142200221400, 0x0000284400442800, 0x0000508800885000, 0x0000a0100010a000, 0x0000402000204000, 
+  0x0002040004020000, 0x0005080008050000, 0x000a1100110a0000, 0x0014220022140000, 0x0028440044280000, 0x0050880088500000, 0x00a0100010a00000, 0x0040200020400000, 
+  0x0204000402000000, 0x0508000805000000, 0x0a1100110a000000, 0x1422002214000000, 0x2844004428000000, 0x5088008850000000, 0xa0100010a0000000, 0x4020002040000000, 
+  0x0400040200000000, 0x0800080500000000, 0x1100110a00000000, 0x2200221400000000, 0x4400442800000000, 0x8800885000000000, 0x100010a000000000, 0x2000204000000000, 
+  0x0004020000000000, 0x0008050000000000, 0x00110a0000000000, 0x0022140000000000, 0x0044280000000000, 0x0088500000000000, 0x0010a00000000000, 0x0020400000000000, 
+};
+
 static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_board_mask_t board_mask, ftk_board_mask_t opponent_mask, ftk_square_e position, ftk_square_e *ep)
 {
   ftk_board_mask_t mask = 0;
@@ -149,13 +161,13 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
     int direction = (square.color == FTK_COLOR_WHITE)
                         ? 1
                         : -1;             // change direction based on color
-    int test = position + direction * 8; // forward/back 1
+    int test = position + (direction << 3); // forward/back 1
 
     if (test < FTK_STD_BOARD_SIZE && test >= 0 &&
         ((board_mask & 1ULL << test) == 0)) {
       mask |= (1ULL << test);
 
-      test = position + direction * 16; // forward/back 2
+      test = position + (direction << 4); // forward/back 2
 
       if ((square.moved == FTK_MOVED_NOT_MOVED) && test < FTK_STD_BOARD_SIZE &&
           test >= 0 && ((board_mask & 1ULL << test) == 0)) {
@@ -168,55 +180,7 @@ static inline ftk_board_mask_t ftk_build_move_mask_raw(ftk_square_s square, ftk_
   } 
   else if (square.type == FTK_TYPE_KNIGHT) 
   {
-    // generate mask if square is a knight
-    int test = position + 17;  // up two and right one
-    if (test < FTK_STD_BOARD_SIZE && test >= 0 && (test % 8) > (position % 8) &&
-        ((board_mask & 1ULL << test) == 0 ||
-         (opponent_mask & 1ULL << test) != 0)) {
-      mask |= (1ULL << test);
-    }
-    test = position - 17; // down two and left one
-    if (test < FTK_STD_BOARD_SIZE && test >= 0 && (test % 8) < (position % 8) &&
-        ((board_mask & 1ULL << test) == 0 ||
-         (opponent_mask & 1ULL << test) != 0)) {
-      mask |= (1ULL << test);
-    }
-    test = position + 15; // up two and left one
-    if (test < FTK_STD_BOARD_SIZE && test >= 0 && (test % 8) < (position % 8) &&
-        ((board_mask & 1ULL << test) == 0 ||
-         (opponent_mask & 1ULL << test) != 0)) {
-      mask |= (1ULL << test);
-    }
-    test = position - 15; // down two and right one
-    if (test < FTK_STD_BOARD_SIZE && test >= 0 && (test % 8) > (position % 8) &&
-        ((board_mask & 1ULL << test) == 0 ||
-         (opponent_mask & 1ULL << test) != 0)) {
-      mask |= (1ULL << test);
-    }
-    test = position + 10; // right two and up one
-    if (test < FTK_STD_BOARD_SIZE && test >= 0 && (test % 8) > (position % 8) &&
-        ((board_mask & 1ULL << test) == 0 ||
-         (opponent_mask & 1ULL << test) != 0)) {
-      mask |= (1ULL << test);
-    }
-    test = position - 10; // left two and down one
-    if (test < FTK_STD_BOARD_SIZE && test >= 0 && (test % 8) < (position % 8) &&
-        ((board_mask & 1ULL << test) == 0 ||
-         (opponent_mask & 1ULL << test) != 0)) {
-      mask |= (1ULL << test);
-    }
-    test = position + 6; // left two and up one
-    if (test < FTK_STD_BOARD_SIZE && test >= 0 && (test % 8) < (position % 8) &&
-        ((board_mask & 1ULL << test) == 0 ||
-         (opponent_mask & 1ULL << test) != 0)) {
-      mask |= (1ULL << test);
-    }
-    test = position - 6; // right two and down one
-    if (test < FTK_STD_BOARD_SIZE && test >= 0 && (test % 8) > (position % 8) &&
-        ((board_mask & 1ULL << test) == 0 ||
-         (opponent_mask & 1ULL << test) != 0)) {
-      mask |= (1ULL << test);
-    }
+    mask |= (knight_move_mask[position] & ((~board_mask) | opponent_mask));
   } 
   else if ((square.type == FTK_TYPE_BISHOP) ||
              (square.type == FTK_TYPE_ROOK) ||
